e046d93f0931433bc1559dbe4699fcc2128a5ec7
itsariadust <jakobaindreas11@gmail.com>
2025-04-07 09:47:21 +0800

    POJOs for table schemas

---
+++ src/main/java/com/itsariadust/qrattendance/Attendance.java
@@ -0,0 +1,46 @@
+package com.itsariadust.qrattendance;
+
+public class Attendance {
+
+  private long attendanceId;
+  private long studentNo;
+  private java.sql.Timestamp timestamp;
+  private String status;
+
+
+  public long getAttendanceId() {
+    return attendanceId;
+  }
+
+  public void setAttendanceId(long attendanceId) {
+    this.attendanceId = attendanceId;
+  }
+
+
+  public long getStudentNo() {
+    return studentNo;
+  }
+
+  public void setStudentNo(long studentNo) {
+    this.studentNo = studentNo;
+  }
+
+
+  public java.sql.Timestamp getTimestamp() {
+    return timestamp;
+  }
+
+  public void setTimestamp(java.sql.Timestamp timestamp) {
+    this.timestamp = timestamp;
+  }
+
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+}
---
+++ src/main/java/com/itsariadust/qrattendance/Students.java
@@ -0,0 +1,66 @@
+package com.itsariadust.qrattendance;
+
+public class Students {
+
+  private long studentNo;
+  private String firstName;
+  private String middleName;
+  private String lastName;
+  private String programId;
+  private String yearLevel;
+
+
+  public long getStudentNo() {
+    return studentNo;
+  }
+
+  public void setStudentNo(long studentNo) {
+    this.studentNo = studentNo;
+  }
+
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+
+  public String getMiddleName() {
+    return middleName;
+  }
+
+  public void setMiddleName(String middleName) {
+    this.middleName = middleName;
+  }
+
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+
+  public String getProgramId() {
+    return programId;
+  }
+
+  public void setProgramId(String programId) {
+    this.programId = programId;
+  }
+
+
+  public String getYearLevel() {
+    return yearLevel;
+  }
+
+  public void setYearLevel(String yearLevel) {
+    this.yearLevel = yearLevel;
+  }
+
+}
